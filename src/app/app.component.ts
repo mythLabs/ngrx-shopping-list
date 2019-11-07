f44645e0f8f8f385c163f12a5804ef0bc585d3d8
mythLabs <amitbisht744@gmail.com>
2019-11-07 10:03:22 +0530

    Added delete item action, reducers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { v4 as uuid } from 'uuid';
+
+
+
 import { ShoppingItem } from './store/models/shopping-item.model';
 import { AppState } from './store/models/app-state.model';
+import { AddItemAction, DeleteItemAction } from './store/actions/shopping.actions';
 
 
 
@@ -14,11 +19,25 @@ import { AppState } from './store/models/app-state.model';
 export class AppComponent implements OnInit {
 
   shoppingItems: Observable<Array<ShoppingItem>>;
+  newShoppingItem: ShoppingItem = { id: '', name: '' };
+
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
     this.shoppingItems = this.store.select(store => store.shopping);
   }
-  
+
+  addItem() {
+    this.newShoppingItem.id = uuid();
+
+    this.store.dispatch(new AddItemAction(this.newShoppingItem));
+
+    this.newShoppingItem = { id: '', name: '' };
+  }
+
+  deleteItem(id: string) {
+    this.store.dispatch(new DeleteItemAction(id));
+  }
+
 }
