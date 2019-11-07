a1e7c307adda38ce60e0038ca1d69cac8f404ec3
mythLabs <amitbisht744@gmail.com>
2019-11-07 09:41:08 +0530

    updated app.module to include store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store'; 
 
 import { AppComponent } from './app.component';
+import { ShoppingReducer } from './store/reducers/shopping.reducers';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({
+      shopping: ShoppingReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
